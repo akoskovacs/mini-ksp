2f500fcc04102e94866af5151c0d44a08b73e967
Ákos Kovács <akoskovacs@gmx.com>
2015-10-19 07:05:11 +0200

    Some useful stuff added:
            * Projectiles (with gravity)
            * The ship is throttled down
            * Made the steering slower

--- main.js
+++ main.js
@@ -148,22 +148,27 @@ window.onload = function() {
             size: 10,//3390e3,  // meter
             mass: 15000//639e21 // Kg
         }],
-        STAR_COUNT = 200;
+        STAR_COUNT     = 200,
+        PROJECTILE_TTL = 100,
+        PROJECTILE_MAX = 5;
    
     var earth   = planets[0],
         mars    = planets[1],
         gravity = vector.create(),
         stars   = [],
         keymap  = [],
+        projectiles = [],
         assets = {
           earth: document.getElementById("planet-earth"),
           rocket: document.getElementById("rocket-noflame"),
           rocket_flame: document.getElementById("rocket-wflame")
         },
-        startxy = vector.create(centerX, centerY-300),
+        startxy  = vector.create(centerX, centerY-300),
+        startvel = vector.createPolar(angle.toRad(0), 1.7),
         ship    = { 
-            pos: startxy, 
-            velocity: vector.createPolar(angle.toRad(0), 1.7),
+            pos: startxy.copy(), 
+            velocity: startvel.copy(),
+            gravity: vector.create(),
             heading: angle.toRad(90),
             oldpos: startxy.copy(),
             img: assets.rocket,
@@ -175,29 +180,80 @@ window.onload = function() {
     onkeydown = onkeyup = function(e) {
       keymap[e.keyCode] = e.type;
     }
+    myship = ship;
 
-    var gravity = vector.createPolar(0, 0);
-    function gravitateTo(planet) {
-      var r   = planet.pos.distanceTo(ship.pos),
-          f   = (planet.mass*ship.mass)/(r*r),
-          phi = ship.pos.angleTo(planet.pos);
+    function gravitateTo(planet, obj) {
+      var r   = planet.pos.distanceTo(obj.pos),
+          f   = (planet.mass*obj.mass)/(r*r),
+          phi = obj.pos.angleTo(planet.pos);
       
-      gravity.setPolar(phi, f);
+      obj.gravity.setPolar(phi, f);
       return gravity;
     }
     
+    function doGravity(planet, obj) {
+      /* Comment out the next line, to disable gravity */
+      gravitateTo(planet, obj);
+      obj.velocity.addTo(obj.gravity);
+      obj.pos.addTo(obj.velocity);
+    }
+    
+    /* Dumb collision detection */
+    function isInCircle(cpos, pos, r) {
+      var dx = pos.x-cpos.x,
+          dy = pos.y-cpos.y;
+      
+      return dx*dx+dy*dy < r*r;
+    }
+  
     function doPhysics() {
-      gv = gravitateTo(earth);
-      ship.velocity.addTo(gv);
-      ship.pos.addTo(ship.velocity);
+      var r_earth = assets.earth.width/2;
+      doGravity(earth, ship);
+      /* Reset ship on collision */
+      if (isInCircle(earth.pos, ship.pos, r_earth+10)) {
+        ship.pos.set(startxy);
+        ship.velocity.set(startvel);
+      }
+      
+      for (var i = 0; i < projectiles.length; i++) {
+        var p = projectiles[i];
+        doGravity(earth, p);
+        if (isInCircle(earth.pos, p.pos, r_earth+5)) {
+          /* If the projectile collided with the planet, delete it from the array.
+             It's a pity, that we have done all these physics on it... :( */
+          projectiles.splice(i, i); 
+        }
+      }
     }
 
-    function drawPixel(x, y, r, g, b, a) {
-      var index = (x + y * width) * 4;    
-      canvasData.data[index + 0] = r;
-      canvasData.data[index + 1] = g;
-      canvasData.data[index + 2] = b;
-      canvasData.data[index + 3] = a;
+    function repositionIfGone(obj, x, y) {
+      if (x <= 0) {
+        obj.pos.x = width;
+      } else if (x > width) {
+        obj.pos.x = 0;
+      }
+      if (y <= 0) {
+        obj.pos.y = height-10;
+      } else if (y >= height) {
+        obj.pos.y = 0;
+      }
+    }
+  
+    function drawProjectiles() {
+      for (var i = 0; i < projectiles.length; i++) {
+        var p = projectiles[i];
+        repositionIfGone(p, p.pos.x, p.pos.y);
+        context.beginPath();
+        context.shadowBlur  = 20;
+        context.fillStyle   = '#ff0';
+        context.shadowColor = 'yellow'; //'#aa0';
+        context.strokeStyle = '#440';
+        context.arc(p.pos.x, p.pos.y, 3, 0, Math.PI*2);
+        context.fill();
+        context.stroke();
+        context.closePath();
+      }
+      context.shadowBlur = 0;
     }
   
     function drawStars() {
@@ -212,6 +268,7 @@ window.onload = function() {
       var halfH = assets.earth.height/2,
           halfW = assets.earth.width/2;
           
+      // These lines could rotate the Earth
       /*
       context.beginPath();
       context.save();
@@ -219,6 +276,8 @@ window.onload = function() {
       context.rotate(earth.rotation);
       context.drawImage(assets.earth, -halfW, -halfH);
       */
+      context.shadowBlur = 30;
+      context.shadowColor = 'blue';
       context.drawImage(assets.earth, earth.pos.x-halfW, earth.pos.y-halfH);
       /*
       context.restore();
@@ -228,8 +287,9 @@ window.onload = function() {
     }
   
     function drawCraft() {
-      var halfW = ship.img.width/2,
+     var halfW = ship.img.width/2+14,
           halfH = ship.img.height/2;
+      //repositionIfGone(ship, ship.pos.x, ship.pos.y); // TODO
       if (ship.pos.x+halfW <= 0) {
         ship.pos.x = width;
       } else if (ship.pos.x > width) {
@@ -240,22 +300,53 @@ window.onload = function() {
       } else if (ship.pos.y > height) {
         ship.pos.y = 0;
       }
+
       context.beginPath();
+      /* Some useful debug shapes for the spacecraft image coordinates */
+      //context.fillRect(ship.pos.x, ship.pos.y, 10, 10);                     // image (0,0) marker
+      //context.arc(ship.pos.x+halfW, ship.pos.y+halfH, halfH, 0, Math.PI*2); // center marker
+      //context.fillRect(ship.pos.x+halfW*2, ship.pos.y+halfH*2, 10, 10);     // marker for (w, h)
       context.save();
+      /* These magic lines, are only here to do rotation on a single image (muh canvas) */
       context.translate(ship.pos.x+halfW, ship.pos.y+halfH);
       context.rotate(ship.heading);
       context.drawImage(ship.img, -halfW, -halfH);
       context.restore();
+      context.fill();
       context.closePath();
+    }
+  
+    function createProjectile() {
+      var proj = {
+        velocity: vector.create(),
+        pos: ship.pos.copy(),
+        gravity: vector.create(),
+        mass: 10,
+        ttl: 100
+      };
+      var phi = ship.heading-(Math.PI/2),
+            v = vector.createPolar(phi, ship.img.height/2); // Position on the nose cone
+      
+      /* Translate the starting position to the ship's current position */
+      proj.pos.addTo(v);
+      /* Correct location to the ship's center point */
+      proj.pos.x += ship.img.width/2+14; // Some magic
+      proj.pos.y += ship.img.height/2;
+      /* Make the projectile move a bit faster than the ship (with the correct heading) */
+      proj.velocity.setPolar(phi, ship.velocity.getLength()+1);
+      if (projectiles.length >= PROJECTILE_MAX) { 
+        projectiles.shift(); // Eliminate space debris
+      }
+      projectiles.push(proj);
     }
    
     function doKeys() {
         if (keymap[37] == 'keydown') { // left
-          ship.heading -= 0.1;
+          ship.heading -= 0.08;
           keymap[37] == undefined;
         }
         if (keymap[38] == 'keydown') { // up
-          var v = vector.createPolar(ship.heading-Math.PI/2, 0.5);
+          var v = vector.createPolar(ship.heading-Math.PI/2, 0.1);
           ship.velocity.addTo(v);
           ship.img = assets.rocket_flame;
           keymap[38] == undefined;
@@ -264,8 +355,12 @@ window.onload = function() {
           keymap[38] == undefined;
         }
         if (keymap[39] == 'keydown') { // right
-          ship.heading += 0.1;
+          ship.heading += 0.08;
           keymap[39] == undefined;
+        }
+        if (keymap[32] == 'keyup') { // space
+          createProjectile();
+          keymap[32] = undefined;
         }
       //keymap = [];
     }
@@ -275,6 +370,7 @@ window.onload = function() {
         doKeys();
         doPhysics();
         drawStars();
+        drawProjectiles();
         drawCraft();
         drawPlanets();
         window.requestAnimationFrame(render);
